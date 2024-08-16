c380f4adbcf36b81ea826bf8126365aa848ef399
Jakob Görgen <jakob.gorgen1@gmx.de>
2024-08-16 11:40:54 +0200

    corundum: updated demo script for more transparency + some simplifications

--- corundum/demo.py
+++ corundum/demo.py
@@ -27,11 +27,14 @@ import simbricks.orchestration.simulators as sim
 from simbricks.orchestration.e2e_topologies import E2EDumbbellTopology
 from simbricks.orchestration.simulator_utils import create_basic_hosts
 
+from functools import reduce
+
 # needed for dot graph
 import sys
 
 sys.path.insert(0, "/workspaces/simbricks-examples/utils")
 from visualize import experiment_graph_e2e_dumbbell
+
 
 # helper method to create the HostClass
 def get_host_class(experiment, host_type):
@@ -48,14 +51,15 @@ def get_host_class(experiment, host_type):
         HostClass = qemu_timing
     elif host_type == "gt":
         HostClass = sim.Gem5Host
-        #e.checkpoint = True
+        # e.checkpoint = True
     else:
         raise NameError(host_type)
     return HostClass
 
+
 # helper method to create NicClass and NodeConfig
+# NOTE: the node is important as it provides drivers etc. needed to interface the NIC
 def get_nic_class_and_node(nic_type):
-    # nic
     NicClass = None
     NcClass = None
     if nic_type == "ib":
@@ -80,137 +84,163 @@ link_latency = 5  # in ms
 bdp = int(link_rate * link_latency / 1000 * 10**6)  # Bandwidth-delay product
 ip_start = "192.168.64.1"
 num_ns3_hosts = 0
-nics_confs = ["cb"] # one of: 'cv', 'cb', 'ib'
-hos_conf = [(1, "gt"), (1, "qt")] # (amount, host_type), host_type one of qt, gt, qemu
+nic_type = "cv"  # one of: 'cv', 'cb', 'ib'
+hos_conf = [
+    (4, "qt")
+]  # (amount, host_type), host_type one of qt, gt, qemu, NOTE: do not mix qemu with other host types
+total_simbricks_hosts = reduce(lambda prev, cur: prev + cur[0], hos_conf, 0)
 
+# sanity check and check sync
+unsynchronized = False
+qemu_hosts = list(filter(lambda e: e[1] == "qemu", hos_conf))
+if len(qemu_hosts) > 0 and len(qemu_hosts) != len(hos_conf):
+    raise Exception("cannot mix synchronized with unsynchronized hosts")
+elif len(qemu_hosts) > 0:
+    unsynchronized = True
 
-# create actual experiment(s)
+# create the actual experiment
 experiments = []
 
-total_simbricks_hosts = 0
+queue_size = int(bdp * 2**1)
+options = {
+    "ns3::TcpSocket::SndBufSize": "524288",
+    "ns3::TcpSocket::RcvBufSize": "524288",
+}
+net = sim.NS3E2ENet()
+net.opt = " ".join([f"--{o[0]}={o[1]}" for o in options.items()])
+
+# create a dumbbell topology
+topology = E2EDumbbellTopology()
+topology.data_rate = f"{link_rate}Mbps"
+topology.delay = f"{link_latency}ms"
+topology.queue_size = f"{queue_size}B"
+topology.mtu = f"{mtu-52}"
+net.add_component(topology)
+
+# create ns3 applications to create background traffic -> mixed fidelity simulation
+for i in range(1, num_ns3_hosts + 1):
+    host = e2e.E2ESimpleNs3Host(f"ns3server-{i}")
+    host.delay = "1us"
+    host.data_rate = f"{link_rate}Mbps"
+    host.ip = f"192.168.64.{i}/24"
+    host.queue_size = f"{queue_size}B"
+    app = e2e.E2EPacketSinkApplication("sink")
+    app.local_ip = "0.0.0.0:5000"
+    app.stop_time = "20s"
+    host.add_component(app)
+    probe = e2e.E2EPeriodicSampleProbe("probe", "Rx")
+    probe.interval = "100ms"
+    probe.file = f"sink-rx-{i}"
+    app.add_component(probe)
+    topology.add_left_component(host)
+
+for i in range(1, num_ns3_hosts + 1):
+    host = e2e.E2ESimpleNs3Host(f"ns3client-{i}")
+    host.delay = "1us"
+    host.data_rate = f"{link_rate}Mbps"
+    host.ip = f"192.168.64.{i+num_ns3_hosts+total_simbricks_hosts}/24"
+    host.queue_size = f"{queue_size}B"
+    app = e2e.E2EBulkSendApplication("sender")
+    app.remote_ip = f"192.168.64.{i}:5000"
+    app.stop_time = "20s"
+    host.add_component(app)
+    topology.add_right_component(host)
+
+# create the actual experiment instance
+e = exp.Experiment(
+    "-".join([h[1] for h in hos_conf])
+    + "-"
+    + nic_type
+    + "-Host-"
+    + f"{total_rate}m"
+)
+e.add_network(net)
+
+# create nic class and node config to use
+NicClass, NcClass = get_nic_class_and_node(nic_type)
+
+# create "proper" simulated servers and clients
+ip_start = num_ns3_hosts + 1
+servers = []
 for hos in hos_conf:
-    total_simbricks_hosts += hos[0]
-simbricks_per_client_rate = int(total_rate / total_simbricks_hosts)
-rate = f"{simbricks_per_client_rate}m"
-for nic_type in nics_confs:
-    queue_size = int(bdp * 2**1)
-    options = {
-        "ns3::TcpSocket::SndBufSize": "524288",
-        "ns3::TcpSocket::RcvBufSize": "524288",
-    }
-    net = sim.NS3E2ENet()
-    net.opt = " ".join([f"--{o[0]}={o[1]}" for o in options.items()])
+    for i in range(0, hos[0]):
+        nic = NicClass()
+        nic.name = nic_type
+        nic.set_network(net)
 
-    topology = E2EDumbbellTopology()
-    topology.data_rate = f"{link_rate}Mbps"
-    topology.delay = f"{link_latency}ms"
-    topology.queue_size = f"{queue_size}B"
-    topology.mtu = f"{mtu-52}"
-    net.add_component(topology)
+        node_config = NcClass()
+        node_config.prefix = 24
+        ip = ip_start + i
+        node_config.ip = f'10.0.{int(ip / 256)}.{ip % 256}'
+        node_config.app = node.NetperfServer()
 
-    # create ns3 applications to create background traffic -> mixed fidelity simulation        
-    for i in range(1, num_ns3_hosts + 1):
-        host = e2e.E2ESimpleNs3Host(f"ns3server-{i}")
-        host.delay = "1us"
-        host.data_rate = f"{link_rate}Mbps"
-        host.ip = f"192.168.64.{i}/24"
-        host.queue_size = f"{queue_size}B"
-        app = e2e.E2EPacketSinkApplication("sink")
-        app.local_ip = "0.0.0.0:5000"
-        app.stop_time = "20s"
-        host.add_component(app)
-        probe = e2e.E2EPeriodicSampleProbe("probe", "Rx")
-        probe.interval = "100ms"
-        probe.file = f"sink-rx-{i}"
-        app.add_component(probe)
-        topology.add_left_component(host)
+        host_class = get_host_class(e, hos[1])
+        server = host_class(node_config)
+        server.name = f's.{hos[1]}.{i}'
 
-    for i in range(1, num_ns3_hosts + 1):
-        host = e2e.E2ESimpleNs3Host(f"ns3client-{i}")
-        host.delay = "1us"
-        host.data_rate = f"{link_rate}Mbps"
-        host.ip = f"192.168.64.{i+num_ns3_hosts+total_simbricks_hosts}/24"
-        host.queue_size = f"{queue_size}B"
-        app = e2e.E2EBulkSendApplication("sender")
-        app.remote_ip = f"192.168.64.{i}:5000"
-        app.stop_time = "20s"
-        host.add_component(app)
-        topology.add_right_component(host)
+        server.add_nic(nic)
+        e.add_nic(nic)
+        e.add_host(server)
 
-    e = exp.Experiment(
-        "-".join([h[1] for h in hos_conf])
-        + "-"
-        + nic_type
-        + "-Host-"
-        + f"{total_rate}m"
-    )
-    e.add_network(net)
+        servers.append(server)
+    ip_start += hos[0]
 
-    # create nic class and node config to use
-    NicClass, NcClass = get_nic_class_and_node(nic_type)
+clients = []
+for hos in hos_conf:
+    for i in range(0, hos[0]):
+        nic = NicClass()
+        nic.name = nic_type
+        nic.set_network(net)
 
-    # create "proper" simulated servers and clients
-    ip_start = num_ns3_hosts + 1
-    servers = []
-    for hos in hos_conf:
-        servers.extend(
-            create_basic_hosts(
-                e,
-                hos[0],
-                f"ser-{hos[1]}",
-                net,
-                NicClass,
-                get_host_class(e, hos[1]),
-                NcClass,
-                node.NetperfServer,
-                ip_start=ip_start,
-            )
-        )
-        ip_start += hos[0]
+        node_config = NcClass()
+        node_config.prefix = 24
+        ip = ip_start + i
+        node_config.ip = f'10.0.{int(ip / 256)}.{ip % 256}'
+        node_config.app = node.NetperfClient()
 
-    clients = []
-    for hos in hos_conf:
-        clients.extend(
-            create_basic_hosts(
-                e,
-                hos[0],
-                f"cli-{hos[1]}",
-                net,
-                NicClass,
-                get_host_class(e, hos[1]),
-                NcClass,
-                node.NetperfClient,
-                ip_start=ip_start, 
-            )
-        )
-        ip_start += hos[0]
+        host_class = get_host_class(e, hos[1])
+        client = host_class(node_config)
+        client.name = f'c.{hos[1]}.{i}'
 
-    for i, server in enumerate(servers, 1):
-        host = e2e.E2ESimbricksHost(f"SB-ser-{i}")
-        host.eth_latency = "1us"
-        host.simbricks_component = server.nics[0]
-        topology.add_left_component(host)
+        client.add_nic(nic)
+        e.add_nic(nic)
+        e.add_host(client)
 
-    for i, client in enumerate(clients, 1):
-        host = e2e.E2ESimbricksHost(f"SB-cli-{i}")
-        host.eth_latency = "1us"
-        host.simbricks_component = client.nics[0]
-        topology.add_right_component(host)
+        clients.append(client)
+    ip_start += hos[0]
 
-    i = 0
-    for cl in clients:
-        cl.node_config.app.server_ip = servers[i].node_config.ip
-        i += 1
+# make e2e-topo aware of other simulated hosts and nics
+for i, server in enumerate(servers, 1):
+    host = e2e.E2ESimbricksHost(f"server-{i}")
+    if unsynchronized:
+        host.sync = e2e.SimbricksSyncMode.SYNC_DISABLED
+    host.eth_latency = "1us"
+    host.simbricks_component = server.nics[0]
+    topology.add_left_component(host)
 
-    # The last client waits for the output printed in other hosts,
-    # then cleanup
-    clients[total_simbricks_hosts - 1].node_config.app.is_last = True
-    clients[total_simbricks_hosts - 1].wait = True
+for i, client in enumerate(clients, 1):
+    host = e2e.E2ESimbricksHost(f"client-{i}")
+    if unsynchronized:
+        host.sync = e2e.SimbricksSyncMode.SYNC_DISABLED
+    host.eth_latency = "1us"
+    host.simbricks_component = client.nics[0]
+    topology.add_right_component(host)
 
-    net.init_network()
+# tell the clients netperf applications about the server ips they should send to
+i = 0
+for cl in clients:
+    cl.node_config.app.server_ip = servers[i].node_config.ip
+    i += 1
 
-    print(e.name)
-    experiments.append(e)
+# The last client waits for the output printed in other hosts,
+# then cleanup
+clients[total_simbricks_hosts - 1].node_config.app.is_last = True
+clients[total_simbricks_hosts - 1].wait = True
+
+net.init_network()
+
+print(e.name)
+experiments.append(e)
 
 
 # visualize simulation topology
